fde584ce6742e92dbaa31377edcd8f2279e20cfa
JIANMO-yhao <yhao95267@gmail.com>
2026-08-30 21:02:56 +0800

    Update PythonTutorials

--- SimulationModeling/StockPrice.py
+++ SimulationModeling/StockPrice.py
@@ -1,19 +1,47 @@
 import numpy as np
+from dataclasses import dataclass
 
-# 模拟股票价格生成函数
-# 公式：dSt/St = μ dt + σ dWt + Jt dNt
-# 其中：Zt服从于正态分布，Jt服从于双向指数分布，Nt服从于泊松分布
-def stock_price_simulation(
-    S0,         # 初始股票价格(如 100.00)
-    T,          # 总模拟时间长度/年(如 1.0)
-    N,          # 总模拟时间步数(如 365)
-    miu,        # 股票年化期望收益率(如 0.12)
-    sigma,      # 正态分布标准差/年化连续波动率(如 0.15)
-    prob_J,     # 双向指数分布正向跳跃概率(如 0.3)
-    miu_J,      # 正向跳跃平均幅度/期望值(如 0.05)
-    alpha,      # 负向跳跃相对倍数(alpha > 1，如 2.5 表示负跳平均幅度为 0.05 * 2.5 = 0.125)
-    lamda,      # 泊松分布强度/年化跳跃频率(如 12)
-    num_paths,  # 模拟路径条数(如 1)
-):
-    # 股票年化期望收益
-    return
+# 封装"金融模型类"+"仿真模拟类"
+@dataclass
+class ModelParams:              # 金融模型类
+    S0:float                    # 初始股票价格(如 100.00)
+    miuS:float                  # 股票年化期望收益率(如 0.12)
+    sigma:float                 # 正态分布标准差/年化连续波动率(如 0.15)
+    probJplus:float             # 双向指数分布正向跳跃概率(如 0.3)
+    miuJ:float                  # 正向跳跃平均幅度/期望值(如 0.05)
+    alpha:float                 # 负向跳跃相对倍数(alpha > 1，如 2.5 表示负跳平均幅度为 0.05 * 2.5 = 0.125)
+    lamda:float                 # 泊松分布强度/年化跳跃频率(如 12)
+@dataclass
+class SimulationParams:         # 仿真模拟类
+    T:float                     # 时间长度/年(如 1.00)
+    N:int                       # 总步数/T
+    num_paths:int               # 仿真次数
+    seed:int | None = None      # 随机种子
+
+# 参数输入设置
+model = ModelParams(            # 金融模型类
+    S0=100,
+    miuS=0.12,
+    sigma=0.15,
+    probJplus=0.3,
+    miuJ=0.05,
+    alpha=1.5,
+    lamda=100
+)
+simulation = SimulationParams(  # 仿真模拟类
+    T=1,
+    N=365,
+    num_paths=1,
+    seed=88
+)
+
+# 股票价格仿真函数
+# 年化期望：d(E[St]) / E[St] = μS dt --> E[St] = S0 * exp(μS * t)
+# 股价生成：dXt = μX dt + σ dWt + Jt dNt | Xt = lnSt
+# 其中：Wt服从正态分布，Jt服从双向指数分布，Nt服从泊松分布
+def stock_price_simulation(model, simulation):
+    dt = simulation.T / simulation.N                # 单位步长代表时间/年
+    print(model.S0)
+    return dt
+
+print(stock_price_simulation(model, simulation))    # 仿真测试
